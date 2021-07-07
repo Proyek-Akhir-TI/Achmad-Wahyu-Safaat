bfded7b75b5a79be8fa186e220c53102787837c2
wahyu safaat <wahyusafaat0506@gmail.com>
2021-07-07 20:32:13 +0700

    add model for response varietas

--- app/src/main/java/com/example/udmitrataniapps/model/auth/PegawaiModel.kt
+++ app/src/main/java/com/example/udmitrataniapps/model/auth/PegawaiModel.kt
@@ -1,4 +1,12 @@
 package com.example.udmitrataniapps.model.auth
 
 class PegawaiModel {
+    lateinit var name: String
+    lateinit var nama_lengkap: String
+    lateinit var  nik: Number
+    lateinit var alamat: String
+    lateinit var telepon: Number
+    lateinit var role: String
+    lateinit var foto: String
+    var id = 0
 }
--- app/src/main/java/com/example/udmitrataniapps/model/pelanggan/VarietasPadi.kt
+++ app/src/main/java/com/example/udmitrataniapps/model/pelanggan/VarietasPadi.kt
@@ -1,4 +1,8 @@
 package com.example.udmitrataniapps.model.pelanggan
 
 class VarietasPadi {
+    var id = 0
+    var nama_varietas = ""
+    var deskripsi_varietas = ""
+    var foto_varietas = ""
 }
